896a99e8187f6009891041e58ad47447c74a2903
Achmad Fahresya Rayhannur <af.rayhannur@gmail.com>
2022-10-30 16:53:45 +0700

    modul2

---
+++ MODUL2 ACHMAD/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<head>
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <!-- NavBar -->
+    <section id="navbar">
+        <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark ">
+            <div class="mx-auto d-flex">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <!-- NavBar "Home" -->
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <!-- NavBar "Booking" -->
+                            <a class="nav-link" href="booking.php">Booking</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+    <section id="content">
+        <div class="container-fluid text-center py-5">
+            <h2>Welcome to EAD Rent</h2>
+            <p>Find your best deal here!</p>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <!-- Ertiga card -->
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="https://d2fgf7u961ce77.cloudfront.net/assets/static/img/variant/Burgundy-Red1.png" class="card-img-top" alt="Mobil Ertiga" style="height:300px">
+                        <div class="card-body">
+                            <h3 class="card-title">Ertiga</h3>
+                            <p class="card-text">Rp 300.000/day</p>
+                            <hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">7 Seater</p><hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">1500CC</p><hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">Matic</p>
+                        </div>
+                            <div class="card-footer">
+                                <a href="booking.php"><button type="button" class="btn btn-primary btn-block"style="width:30%">Book Now</button></a>
+                            </div>
+                    </div>
+                </div>
+                <!-- Brio card -->
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="https://asset.honda-indonesia.com/variants/images/Vos4HUL6XFLHnM2dhTzGbzdNHpvNTnBNBKhZwxc0.png" class="card-img-top" alt="Mobil Brio" style="height:300px">
+                        <div class="card-body">
+                            <h3 class="card-title">Honda Brio</h3>
+                            <p class="card-text">Rp 300.000/day</p>
+                            <hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">5 Seater</p><hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">1200CC</p><hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">Matic</p>
+                        </div>
+                            <div class="card-footer">
+                                <a href="booking.php"><button type="button" class="btn btn-primary btn-block"style="width:30%">Book Now</button></a>
+                            </div>
+                    </div>
+                </div>
+                <!-- Jazz card -->
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="https://news.olx.co.id/wp-content/uploads/2021/10/30ff6591-29c8-40be-a561-f0aaf22b194d.png" class="card-img-top" alt="Mobil Ayla" style="height:300px">
+                        <div class="card-body">
+                            <h3 class="card-title">Honda Jazz</h3>
+                            <p class="card-text">Rp 300.000/day</p>
+                            <hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">5 Seater</p><hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">1200CC</p><hr>
+                            <p style="color:blue; font-size:15px; font-weight:bold">Matic</p>
+                        </div>
+                            <div class="card-footer">
+                                <a href="booking.php"><button type="button" class="btn btn-primary btn-block"style="width:30%">Book Now</button></a>
+                            </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </section>
+<footer class="bg-light py-2 text-center"><p class="text-secondary m-0">Created by Achmad_1202204161</p></footer>
+</body>
+</html>
